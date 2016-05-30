106d2edecc54094b1f9dcf967dc0a9557036d5e4
Andre Kowalewski <a.kowa@gmx.de>
2016-05-30 19:18:25 +0200

    ADDED welcome scene and first logic

---
+++ SmartPhoneTrainer/Assets/Scripts/CircleController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleController : MonoBehaviour
+{
+	private RectTransform rect;
+
+	private void Awake ()
+	{
+		rect = GetComponent<RectTransform>();
+	}
+
+	private void Update ()
+	{
+
+		if (Input.touchCount <= 0) return;
+
+		if (Input.touchCount > 1)
+		{
+			Debug.Log ( "Too many fingers." );
+			return;
+		}
+
+		var touch = Input.GetTouch ( 0 );
+
+		if (touch.phase == TouchPhase.Began)
+		{
+			var distance = Vector2.Distance(touch.position, this.transform.position);
+			if (distance - rect.rect.height/2 < 0)
+			{
+				Debug.Log("Tapped");
+			}
+			else
+			{
+				Debug.Log("Outside");
+			}
+		}
+	}
+}
---
+++ SmartPhoneTrainer/Assets/Scripts/WelcomeScreenController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Animator))]
+public class WelcomeScreenController : MonoBehaviour
+{
+	private Animator anim;
+
+	public void Awake ()
+	{
+		this.anim = GetComponent<Animator> ();
+		var state = anim.GetCurrentAnimatorStateInfo(0);
+		StartCoroutine ( LoadAfterTime(state.length) );
+	}
+
+	private IEnumerator LoadAfterTime( float t )
+	{
+		yield return new WaitForSeconds(t);
+		SceneManager.LoadScene(1);
+	}
+}
